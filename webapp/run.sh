b85edd5869b94112b96dc6ab3527a62972f648d9
Daniel-Tomov <90072819+Daniel-Tomov@users.noreply.github.com>
2025-03-05 03:36:38 +0000

    feat: update webapp run.sh to use git

--- webapp/run.sh
+++ webapp/run.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+apt install git curl -y
+
 mkdir /usr/src/ -p
 cd /usr/src/
-wget https://github.com/Daniel-Tomov/Cloud/archive/refs/heads/main.zip -O cloud.zip
-
-unzip cloud.zip
-cd Cloud-main
+#wget https://github.com/Daniel-Tomov/Cloud/archive/refs/heads/main.zip -O cloud.zip
+git clone https://github.com/Daniel-Tomov/Cloud
+#unzip cloud.zip
+#cd Cloud-main
+cd cloud
 cd webapp
 
 pip3 install -r requirements.txt
