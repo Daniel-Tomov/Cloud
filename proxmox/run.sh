2d3d2839df5de131bb2808c048f5df9775c630ad
Daniel-Tomov <90072819+Daniel-Tomov@users.noreply.github.com>
2025-03-07 22:06:35 +0000

    fix: proxmox webapp does not create certificate

--- proxmox/run.sh
+++ proxmox/run.sh
@@ -11,7 +11,7 @@ git clone https://github.com/Daniel-Tomov/Cloud
 cd Cloud
 cd proxmox
 
-echo -e "\n\n\n\n\n\n" | openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -sha256 -days 365 -nodes
+echo -e "\n\n\n\n\n\n" | openssl req -x509 -newkey rsa:4096 -keyout /usr/src/Cloud/proxmox/key.pem -out /usr/src/Cloud/proxmox/cert.pem -sha256 -days 365 -nodes
 
 pip3 install -r requirements.txt
 python3 main.py
