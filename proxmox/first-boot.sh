73f736536afb018ce48bb7b6c74dc1083412b1e8
Daniel-Tomov <90072819+Daniel-Tomov@users.noreply.github.com>
2025-04-08 19:31:47 +0000

    fix: move hosts service before first-boot network commands

--- proxmox/first-boot.sh
+++ proxmox/first-boot.sh
@@ -34,12 +34,6 @@ source /etc/network/interfaces.d/*
 EOF
 
 ifreload -a
-dhclient -r vmbr0
-dhclient vmbr0
-dhclient -r vmbr0
-dhclient vmbr0
-dhclient -r vmbr0
-dhclient vmbr0
 
 sleep 5
 
@@ -47,24 +41,15 @@ echo "#" $(cat /etc/apt/sources.list.d/pve-enterprise.list ) > /etc/apt/sources.
 echo "#" $(cat /etc/apt/sources.list.d/ceph.list ) > /etc/apt/sources.list.d/ceph.list # disable ceph repository
 echo "deb http://download.proxmox.com/debian/pve bookworm pve-no-subscription" > /etc/apt/sources.list.d/pve-community.list # enable community repository
 
-apt update
-apt install curl net-tools -y # basic tools
-
-mkdir -p /var/lib/vz/template/iso
-cd /var/lib/vz/template/iso
-
-#{{replace_with_vms}}
-
-apt-get install qemu-guest-agent -y
-
 # change ip in /ets/hosts
 cp /etc/hosts /etc/hosts.backup
 
 echo "#!/bin/bash" >> /etc/create_hosts_file.sh
+echo 'dhclient vmbr0; dhclient -r vmbr0; dhclient vmbr0' >> /etc/create_hosts_file.sh
 echo "old_ip=\$(cat /etc/hosts.backup | head -n 2 | tail -n 1 | awk '{print \$1}')" >> /etc/create_hosts_file.sh
 echo "new_ip=\$(ip a | grep 'vmbr0: ' -A 2 | tail -n 1 | awk '{print \$2}' | awk -F '/' '{print \$1}')" >> /etc/create_hosts_file.sh
 echo 'cat /etc/hosts.backup | sed "s/$old_ip/$new_ip/g" > /etc/hosts' >> /etc/create_hosts_file.sh
-echo 'dhclient vmbr0; dhclient -r vmbr0; dhclient vmbr0" > /etc/hosts' >> /etc/create_hosts_file.sh
+chmod +x /etc/create_hosts_file.sh
 
 cat <<EOF > /etc/systemd/system/hosts_file.service
 [Unit]
@@ -83,6 +68,17 @@ EOF
 systemctl daemon-reload
 systemctl enable hosts_file
 systemctl start hosts_file
+
+apt update
+apt install curl net-tools -y # basic tools
+
+
+mkdir -p /var/lib/vz/template/iso
+cd /var/lib/vz/template/iso
+
+#{{replace_with_vms}}
+
+apt-get install qemu-guest-agent -y
 
 systemctl start qemu-guest-agent
 systemctl enable qemu-guest-agent
