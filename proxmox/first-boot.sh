9e01d27cb88c67ceeaaa7c39653444d0d4028b41
Daniel-Tomov <90072819+Daniel-Tomov@users.noreply.github.com>
2025-04-08 18:38:14 +0000

    fix: errors in first-boot.sh

--- proxmox/first-boot.sh
+++ proxmox/first-boot.sh
@@ -56,24 +56,24 @@ cd /var/lib/vz/template/iso
 #{{replace_with_vms}}
 
 apt-get install qemu-guest-agent -y
-systemctl start qemu-guest-agent
-systemctl enable qemu-guest-agent
 
 # change ip in /ets/hosts
 cp /etc/hosts /etc/hosts.backup
 
+echo "#!/bin/bash" >> /etc/create_hosts_file.sh
 echo "old_ip=\$(cat /etc/hosts.backup | head -n 2 | tail -n 1 | awk '{print \$1}')" >> /etc/create_hosts_file.sh
 echo "new_ip=\$(ip a | grep 'vmbr0: ' -A 2 | tail -n 1 | awk '{print \$2}' | awk -F '/' '{print \$1}')" >> /etc/create_hosts_file.sh
 echo 'cat /etc/hosts.backup | sed "s/$old_ip/$new_ip/g" > /etc/hosts' >> /etc/create_hosts_file.sh
+echo 'dhclient vmbr0; dhclient -r vmbr0; dhclient vmbr0" > /etc/hosts' >> /etc/create_hosts_file.sh
 
 cat <<EOF > /etc/systemd/system/hosts_file.service
 [Unit]
+Description=Recreate Hosts File after DHCP
 After=network-online.target
 Wants=network-online.target
 
 [Service]
-Type=notify
-WorkingDirectory=/etc
+Type=simple
 ExecStart=/etc/create_hosts_file.sh
 RestartSec=10s
 
@@ -83,3 +83,6 @@ EOF
 systemctl daemon-reload
 systemctl enable hosts_file
 systemctl start hosts_file
+
+systemctl start qemu-guest-agent
+systemctl enable qemu-guest-agent
